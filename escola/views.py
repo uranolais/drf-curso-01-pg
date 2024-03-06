3390d51569112828d5d33b53b003757a5ac6f854
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-06 19:19:09 -0300

    adicionando viewsets e rotas

--- escola/views.py
+++ escola/views.py
@@ -1,10 +1,11 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer,CursoSerializer
 
-def estudantes(request):
-    if request.method == 'GET':
-        estudante = {
-            'id':1,
-            'nome': 'Lais'
-            }
-        print(JsonResponse(estudante))
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import estudantes
+from django.urls import path,include
+from rest_framework import routers
+from escola.views import EstudanteViewSet,CursoViewSet
+
+router = routers.DefaultRouter()
+router.register('estudantes',EstudanteViewSet,basename='Estudantes')
+router.register('cursos',CursoViewSet,basename='Cursos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estudantes/',estudantes)
+    path('',include(router.urls))
 ]

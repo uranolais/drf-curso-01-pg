e32133df00311dcc47dfe2dc25c958950354464c
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-13 17:15:52 -0300

    adicionando versionamento da aula_02

--- escola/views.py
+++ escola/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets, generics, filters  
 from escola.models import Estudante, Curso, Matricula
-from escola.serializers import EstudanteSerializer,CursoSerializer,MatriculaSerializer,ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
+from escola.serializers import EstudanteSerializer,CursoSerializer,MatriculaSerializer,ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer, EstudanteSerializerV2
 from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
-    serializer_class = EstudanteSerializer
+    #serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
     search_fields = ['nome','cpf']
+    def get_serializer_class(self):
+        if self.request.version == 'v2': #rota: http://127.0.0.1:8000/estudantes/?version=v2
+            return EstudanteSerializerV2
+        return EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
--- setup/settings.py
+++ setup/settings.py
@@ -134,4 +134,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
 }

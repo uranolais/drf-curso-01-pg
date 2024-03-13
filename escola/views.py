8f1a02a1064590411e21c43bb2a0ae3ca978b994
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-12 22:18:55 -0300

    adicionando filtros

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,14 @@
-from rest_framework import viewsets, generics
-from escola.models import Estudante, Curso,Matricula
+from rest_framework import viewsets, generics, filters  
+from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer,CursoSerializer,MatriculaSerializer,ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend,filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome','cpf']
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
